ef1b0b44cd9585b26d05c5e378cd81aa7e52285c
dreusswig <dennis.reusswig@gmx.net>
2021-12-06 21:31:09 +0100

    Added functions and comments
    Put algorithms for counting number of increasing values and
    creating sliding window results in functions countIncreasedValues
    and slidingWindow
    Added comments in code

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -6,21 +6,22 @@
 using namespace std;
 
 int countIncreasedValues(vector<int> input);
+vector<int> slidingWindow(vector<int> input, int windowSize);
 
 int main()
 {
     ifstream input(".\\Day1\\input.txt");
     string line;
-    vector<int> input_int;
-    vector<int> input_sliding_window;
+    vector<int> input_int;                      //Vector for reading input file
+    vector<int> input_sliding_window;           //Vector for sums of sliding window approach
     int input_size = 0;
     int result_part1 = 0;
     int result_part2 = 0;
-    int sum = 0;
 
     input_int.reserve(1000);
     input_sliding_window.reserve(1000);
 
+    //Read input from file into vector
     if(input.is_open())
     {
         while(!input.eof())
@@ -32,30 +33,22 @@ int main()
     }
     else cout << "Unable to open input file." << endl;
 
+    //Output number of elements in vector to terminal
     input_size = input_int.size();
-    cout << input_size << endl;
+    cout << "Number of elements in vector read from file: " << input_size << endl;
 
-    // for(int i = 0; i < input_size - 1; i++)
-    // {
-    //     if(input_int[i+1] > input_int[i])
-    //     {
-    //         result_part1++;
-    //     }
-    // }
-
+    //Get result for part 1 (number of increasing values in raw measurement data)
     result_part1 = countIncreasedValues(input_int);
 
-    for(int j = 0; j < input_size - 2; j++)
-    {
-        sum = input_int[j] + input_int[j+1] + input_int[j+2];
-        input_sliding_window.push_back(sum);
-    }
+    //Calculate sliding window result data on raw measurement data
+    input_sliding_window = slidingWindow(input_int, 3);
 
+    //Get result for part 2 (number of increasing values in smoothened data from sliding window)
     result_part2 = countIncreasedValues(input_sliding_window);
 
-    cout << result_part1 << endl;
-    cout << result_part2 << endl;
-
+    //Output results for part 1 & 2 to terminal
+    cout << "Result of Part 1 (Raw Data): " << result_part1 << endl;
+    cout << "Result of Part 2 (Smoothened Data): " << result_part2 << endl;
 }
 
 int countIncreasedValues(vector<int> input)
@@ -67,6 +60,22 @@ int countIncreasedValues(vector<int> input)
         {
             result++;
         }
+    }
+    return result;
+}
+
+vector<int> slidingWindow(vector<int> input, int windowSize)
+{
+    int sum = 0;
+    vector<int> result;
+    for(int j = 0; j < input.size() - (windowSize - 1); j++)
+    {
+        sum = 0;
+        for(int k = 0; k < windowSize; k++)
+        {
+            sum += input[j+k];
+        }
+        result.push_back(sum);
     }
     return result;
 }
